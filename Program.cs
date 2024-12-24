016b00c7dced58bd4330b78402159da239c85aec
Bug1642 <hasancan1642@hotmail.com>
2024-12-24 13:51:21 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ArrayManipulation
+{
+    public class StudentCode
+    {
+        public int[] DoubleArray(int[] numbers)
+        {
+            int[] results = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                results[i] = numbers[i] * 2;
+            }
+            return results;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3 };
+            StudentCode studentCode = new StudentCode();
+            int[] results = studentCode.DoubleArray(numbers);
+            Console.WriteLine("Results: ");
+            foreach (var result in results)
+            {
+                Console.Write(result + " ");
+            }
+        }
+    }
+}
